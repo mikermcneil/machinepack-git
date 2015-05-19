b74c195022d28e842eded5017de674aad05d54a5
Mike McNeil <mike@balderdash.co>
2015-05-19 10:24:35 -0500

    more exits for 'pull' machine

--- machines/pull.js
+++ machines/pull.js
@@ -45,6 +45,15 @@ module.exports = {
       description: 'Specified directory does not exist.'
     },
 
+    uncommittedChanges: {
+      description: 'Cannot pull because uncommitted/unstashed local changes exist locally and would be overwritten by merge.'
+    },
+
+    unresolvedConflicts: {
+      description: 'Cannot pull because this local repo has unmerged conflicts.',
+      extendedDescription: 'To get past this, one would need to resolve the merge conflicts manually, commit the changes (`git commit -am "..your msg.."`), then run this machine again.'
+    },
+
     success: {
       example: 'Already up-to-date.'
     }
@@ -61,25 +70,58 @@ module.exports = {
     }, {
       error: function (err) {
         try {
+          console.log(err, Object.keys(err), err.signal, err.killed, err.code);
           if (err.message.match(/Not a git repository/)) {
             return exits.notRepo();
           }
-          /*
-          Error: Command failed: From github.com:mikermcneil/machinepack-git
- * branch            master     -> FETCH_HEAD
-   874fab0..afa9e77  master     -> origin/master
-error: Your local changes to the following files would be overwritten by merge:
-  README.md
-Please, commit your changes or stash them before you can merge.
-Aborting
+          if (err.message.match(/Your local changes to the following files would be overwritten by merge/) || err.message.match(/Please, commit your changes or stash them before you can merge/)) {
+            return exits.uncommittedChanges();
+            // TODO: attempt to parse out a list of the files which would be overwritten by merge
+            /*
+            Error: Command failed: From github.com:mikermcneil/machinepack-git
+             * branch            master     -> FETCH_HEAD
+               874fab0..afa9e77  master     -> origin/master
+            error: Your local changes to the following files would be overwritten by merge:
+              README.md
+            Please, commit your changes or stash them before you can merge.
+            Aborting
 
-    at ChildProcess.exithandler (child_process.js:637:15)
-    at ChildProcess.EventEmitter.emit (events.js:98:17)
-    at maybeClose (child_process.js:743:16)
-    at Socket.<anonymous> (child_process.js:956:11)
-    at Socket.EventEmitter.emit (events.js:95:17)
-    at Pipe.close (net.js:465:12)
-           */
+                at ChildProcess.exithandler (child_process.js:637:15)
+                at ChildProcess.EventEmitter.emit (events.js:98:17)
+                at maybeClose (child_process.js:743:16)
+                at Socket.<anonymous> (child_process.js:956:11)
+                at Socket.EventEmitter.emit (events.js:95:17)
+                at Pipe.close (net.js:465:12)
+             */
+          }
+
+          if (err.message.match(/Pull is not possible because you have unmerged files/i)) {
+            return exits.unresolvedConflicts();
+             /*
+             Error: Command failed: Pull is not possible because you have unmerged files.
+              Please, fix them up in the work tree, and then use 'git add/rm <file>'
+              as appropriate to mark resolution, or use 'git commit -a'.
+
+                  at ChildProcess.exithandler (child_process.js:637:15)
+                  at ChildProcess.EventEmitter.emit (events.js:98:17)
+                  at maybeClose (child_process.js:743:16)
+                  at Socket.<anonymous> (child_process.js:956:11)
+                  at Socket.EventEmitter.emit (events.js:95:17)
+                  at Pipe.close (net.js:465:12)
+              */
+          }
+
+
+           /*
+           Error: Command failed: From github.com:mikermcneil/machinepack-git
+             * branch            master     -> FETCH_HEAD
+
+                at ChildProcess.exithandler (child_process.js:637:15)
+                at ChildProcess.EventEmitter.emit (events.js:98:17)
+                at maybeClose (child_process.js:743:16)
+                at Process.ChildProcess._handle.onexit (child_process.js:810:5)
+            */
+           // ========== ==================== ========== and then ========== ==================== ==========
         }
         catch (e){}
         return exits.error(err);
